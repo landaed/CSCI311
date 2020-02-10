118ce13e6d2c9302e2a939594a8016dd98251e10
admin <admin@admins-MacBook-Air.local>
2020-02-10 11:48:54 -0800

    abstracted to its own folder

--- public/assets/js/game.js
+++ public/assets/js/game.js
@@ -1,50 +1,58 @@
-let game, scores;
+var myGamePiece;
 
-class Highscore extends Phaser.Scene {
-
-  constructor() {
-    super({
-      key: 'Highscore',
-      active: true
-    });
-
-    this.scores = [];
-  }
-
-  preload() {
-    this.load.bitmapFont('arcade', 'assets/arcade.png', 'assets/arcade.xml');
-  }
-
-  create() {
-    this.add.bitmapText(100, 110, 'arcade', 'RANK  SCORE   NAME').setTint(0xffffff);
-
-    for (let i = 1; i < 6; i++) {
-      if (scores[i-1]) {
-        this.add.bitmapText(100, 160 + 50 * i, 'arcade', ` ${i}      ${scores[i-1].highScore}    ${scores[i-1].name}`).setTint(0xffffff);
-      } else {
-        this.add.bitmapText(100, 160 + 50 * i, 'arcade', ` ${i}      0    ---`).setTint(0xffffff);
-      }
-    }
-  }
+function startGame() {
+    myGameArea.start();
+    myGamePiece = new component(30, 30, "red", 10, 120);
 }
 
-let config = {
-  type: Phaser.AUTO,
-  parent: 'phaser-example',
-  width: 800,
-  height: 600,
-  pixelArt: true,
-  scene: [Highscore]
-};
+var myGameArea = {
+    canvas : document.createElement("canvas"),
+    start : function() {
+        this.canvas.width = 480;
+        this.canvas.height = 270;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.interval = setInterval(updateGameArea, 20);
+        window.addEventListener('keydown', function (e) {
+            myGameArea.keys = (myGameArea.keys || []);
+            myGameArea.keys[e.keyCode] = (e.type == "keydown");
+        })
+        window.addEventListener('keyup', function (e) {
+            myGameArea.keys[e.keyCode] = (e.type == "keydown");
+        })
+    },
+    clear : function(){
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+}
 
-$.ajax({
-  type: 'GET',
-  url: '/scores',
-  success: function(data) {
-    game = new Phaser.Game(config);
-    scores = data;
-  },
-  error: function(xhr) {
-    console.log(xhr);
-  }
-});
+function component(width, height, color, x, y) {
+    this.gamearea = myGameArea;
+    this.width = width;
+    this.height = height;
+    this.speedX = 0;
+    this.speedY = 0;
+    this.x = x;
+    this.y = y;
+    this.update = function() {
+        ctx = myGameArea.context;
+        ctx.fillStyle = color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+    this.newPos = function() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+    }
+}
+
+function updateGameArea() {
+    myGameArea.clear();
+    myGamePiece.speedX = 0;
+    myGamePiece.speedY = 0;
+    if (myGameArea.keys && myGameArea.keys[37]) {myGamePiece.speedX = -1; }
+    if (myGameArea.keys && myGameArea.keys[39]) {myGamePiece.speedX = 1; }
+    if (myGameArea.keys && myGameArea.keys[38]) {myGamePiece.speedY = -1; }
+    if (myGameArea.keys && myGameArea.keys[40]) {myGamePiece.speedY = 1; }
+    myGamePiece.newPos();
+    myGamePiece.update();
+}
